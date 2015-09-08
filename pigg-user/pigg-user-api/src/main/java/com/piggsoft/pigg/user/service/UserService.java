7ac6ed71a2e1d631ebe08dacabfdf1341d8ab3f8
pigg <piggsoft@gmail.com>
2015-09-08 14:33:03 +0800

    modify configuration

--- pigg-user/pigg-user-api/src/main/java/com/piggsoft/pigg/user/service/UserService.java
+++ pigg-user/pigg-user-api/src/main/java/com/piggsoft/pigg/user/service/UserService.java
@@ -1,10 +1,16 @@
 package com.piggsoft.pigg.user.service;
 
 import com.piggsoft.pigg.user.model.User;
+import com.piggsoft.pigg.user.model.UserExample;
+
+import java.util.List;
 
 public interface UserService {
 	int save(User user);
 	User find(Long userId);
-	User find(User user);
-	User find();
+	List<User> findByExample(UserExample example);
+	List<User> find();
+	int updateUserByExample(User user, UserExample example);
+	int deleteById(Long userId);
+	int deleteByUserExample(UserExample example);
 }
--- pigg-user/pigg-user-provider/src/main/java/com/piggsoft/pigg/user/service/impl/UserServiceImpl.java
+++ pigg-user/pigg-user-provider/src/main/java/com/piggsoft/pigg/user/service/impl/UserServiceImpl.java
@@ -1,37 +1,57 @@
 package com.piggsoft.pigg.user.service.impl;
 
+import com.piggsoft.pigg.user.model.UserExample;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.piggsoft.pigg.user.dao.UserMapper;
 import com.piggsoft.pigg.user.model.User;
 import com.piggsoft.pigg.user.service.UserService;
 
+import java.util.List;
+
 public class UserServiceImpl implements UserService {
-	
-	@Autowired
-	private UserMapper userMapper;
 
-	public int save(User user) {
-		return userMapper.insert(user);
-	}
+    @Autowired
+    private UserMapper userMapper;
 
-	public User find(Long userId) {
-		User user = userMapper.selectByPrimaryKey(Integer.parseInt(userId.toString()));
-		return user;
-	}
+    @Override
+    public int save(User user) {
+        return userMapper.insert(user);
+    }
 
-	public User find(User user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public User find(Long userId) {
+        User user = userMapper.selectByPrimaryKey(Integer.parseInt(userId.toString()));
+        return user;
+    }
 
-	public User find() {
-		System.out.println("find");
-		return null;
-	}
+    @Override
+    public List<User> findByExample(UserExample example) {
+        return userMapper.selectByExample(example);
+    }
 
-	public void setUserMapper(UserMapper userMapper) {
-		this.userMapper = userMapper;
-	}
+    @Override
+    public List<User> find() {
+        return userMapper.selectByExample(new UserExample());
+    }
+
+    @Override
+    public int updateUserByExample(User user, UserExample example) {
+        return 0;
+    }
+
+    @Override
+    public int deleteById(Long userId) {
+        return 0;
+    }
+
+    @Override
+    public int deleteByUserExample(UserExample example) {
+        return 0;
+    }
+
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
 
 }
